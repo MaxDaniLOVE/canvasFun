105146dd6dd377e367481a60fe0b2a4498f4fe5b
MaxDaniLOVE <mahseeen@gmail.com>
2019-10-28 14:50:02 +0300

    feat: add woo function

--- src/script.js
+++ src/script.js
@@ -2,8 +2,10 @@ let canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
     moon = new Image(),
     town_far = new Image(),
-    sky_bg = new Image()
-    ground_bg = new Image();
+    ground_bg = new Image(),
+    woo = new Image(),
+    sky_bg = new Image();
+    
 var bgMove = [{
     X: 0,
     Y: 0
@@ -16,15 +18,19 @@ var bgGround = [{
     X: 0,
     Y: canvas.height - 150
 }] 
+var woomove = [{
+    X: 20,
+    Y: canvas.height - 110
+}]
 
 let difficulty = 1;
 
     ground_bg.src = "images/ground_bg.png" 
     town_far.src = "images/town_far.png"    
     moon.src = "images/moon.png"
+    woo.src = "images/woo.png"
     sky_bg.src = "images/sky_bg.png"
     
-
 
     function draw(){
         drawBg(sky_bg, bgMove, 0, 0.05) 
@@ -32,13 +38,18 @@ let difficulty = 1;
         drawBg(ground_bg, bgGround, canvas.height - 150, 1)
 
         ctx.drawImage(moon, 20, 20)
-
-
-
-
+        wooWalk()
+        
         requestAnimationFrame(draw)
     }
-    
+
+    /*
+     ! ATTENTION               
+     * @param {*} image --- image that should be shown
+     * @param {*} newArr --- array to push another image that should be shown
+     * @param {*} yStartPositionBg --- starting position on axis Y to show image
+     ! @param {*} animationSpeed --- 0.05 /// 0.25 /// 0.5 /// 1       
+     */
 
     function drawBg(image, newArr, yStartPositionBg, animationSpeed) {
         for (let i = 0; i < newArr.length; i++) {
@@ -50,6 +61,16 @@ let difficulty = 1;
             }
             ctx.drawImage(image, newArr[i].X, newArr[i].Y)
             newArr[i].X -= animationSpeed
+        }
+    }
+
+    /* 
+    TODO: add normal function 
+    */
+
+    function wooWalk() {
+        for(i = 0; i < 10; i++){
+            ctx.drawImage(woo, woomove[0].X,  woomove[0].Y)
         }
     }
     sky_bg.onload = draw;
